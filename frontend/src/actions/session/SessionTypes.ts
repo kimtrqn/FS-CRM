fe7e9975d71c8fd609fa5ab9843305bd8482feca
Julie Tran <kimtran767@gmail.com>
2022-01-15 19:57:48 -0800

    added payload withtin interface for session

--- frontend/src/actions/session/SessionTypes.ts
+++ frontend/src/actions/session/SessionTypes.ts
@@ -3,9 +3,31 @@ export const LOGOUT_USER = 'LOGOUT_USER';
 export const SESSION_ERRORS = "SESSION_ERRORS";
 export const CLEAR_SESSION_ERRORS = "CLEAR_SESSION_ERRORS";
 
+type LoginInfo = {
+    email: string,
+    passsword: string
+};
+
+type SessionErrorsArr = {
+    errors: []
+};
 
 interface LoginUser {
-    type: typeof LOGIN_USER
+    type: typeof LOGIN_USER,
+    payload: LoginInfo
+};
+
+interface LogoutUser {
+    type: typeof LOGOUT_USER
+};
+
+interface SessionErrors {
+    type: typeof SESSION_ERRORS
+    payload: SessionErrorsArr
+};
+
+interface ClearSessionErrors {
+    type: typeof CLEAR_SESSION_ERRORS
 };
 
 
